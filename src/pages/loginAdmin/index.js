a57cba9ca3e43d47db45a3e20533ced6e36f105b
unknown <ardila0212@gmail.com>
2024-05-24 16:23:46 +0200

    robots

--- src/pages/loginAdmin/index.js
+++ src/pages/loginAdmin/index.js
@@ -6,18 +6,13 @@ import LoginDashboard from "./LoginDashboard";
 
 export default function FormDashboard() {
     return (
-        <>
-            
-            <Head>
-        <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0  minimum-scale=1.0" />
-        <link rel="icon" href="/pinocho.png" type="image/png"/>
-        <title>ADMIN PINOCHO</title>
         
-        <meta name="robots" content="noindex, nofollow"/>
-      </Head>
+            
+           
+        
+     
             <LoginDashboard/>
-        </>
+        
     );
 }
 
