8e333204fd295a39dfe316fefb714bc430b8a805
unknown <ardila0212@gmail.com>
2024-05-25 13:28:04 +0200

    img

--- src/pages/ofertas/ofertas.js
+++ src/pages/ofertas/ofertas.js
@@ -71,7 +71,7 @@ const Oferts = () => {
             <p className={styles.textcol}>
             ¡Ven a desayunar en Restaurante Pinocho! Nuestra deliciosa selección de bizcochos recién horneados, tostadas crujientes, minis tentadores y croissants irresistibles harán que su mañana sea simplemente deliciosa. Y qué mejor manera de acompañar estos manjares que con una buena taza de café, un zumo de naranja exprimido al momento o una deliciosa infusión. Ya sea que estén buscando un desayuno rápido entre semana o quieran disfrutar de una agradable mañana de fin de semana, ¡nosotros tenemos todo lo que necesitan para comenzar el día con el pie derecho! ¡Los esperamos con los brazos abiertos en Restaurante Pinocho para una experiencia de desayuno que nunca olvidarán!
             </p>
-            <spam className={styles.precio}>De Lunes a Viernes 3 euros, Sábados, Domingos y festivos 3,30 euros</spam>
+            <span className={styles.precio}>De Lunes a Viernes 3 euros, Sábados, Domingos y festivos 3,30 euros</span>
            
           </div>
         </div>
@@ -90,7 +90,7 @@ const Oferts = () => {
             {parrafos.filter((_, index) => index >= 3 && index < 6).map(parrafo => (
               <p key={parrafo.id} className={styles.textcol}>{parrafo.contenido}</p>
             ))}
-          <spam className={styles.precio}>Todos los días tu plato del día por 9,90€. También puedes disfrutar de solo el plato principal por 7,00€ </spam>
+          <span className={styles.precio}>Todos los días tu plato del día por 9,90€. También puedes disfrutar de solo el plato principal por 7,00€ </span>
           </div>
         </div>
     </div>
