e4ae68aac590c395093172ec42637f36cb038eb6
unknown <ardila0212@gmail.com>
2024-06-07 13:36:27 +0200

    reservations

--- src/pages/gestionReservas/gestionReservas.js
+++ src/pages/gestionReservas/gestionReservas.js
@@ -18,8 +18,8 @@ const GestionReservas = () => {
             });
     }, []);
 
-    const handleAceptar = (id, score) => {
-      axios.put(`https://api.restaurantepinochozaragoza.es/api/reservations/${id}/accept`, { status: 'aceptado', score })
+    const handleAceptar = (id) => {
+      axios.put(`https://api.restaurantepinochozaragoza.es/api/reservations/${id}/accept`, { status: 'aceptado' })
           .then(response => {
               console.log('Reserva aceptada:', response.data);
               Swal.fire(
@@ -27,16 +27,16 @@ const GestionReservas = () => {
                 'Reserva Aceptada con éxito',
                 'Recibirás un correo con la información de la reserva',
               );
+              setReservasPendientes(reservasPendientes.filter(reserva => reserva.id !== id));
               getAceptadas();
-              getRechazadas();
           })
           .catch(error => {
               console.error('Error al aceptar la reserva:', error);
           });
   };
   
-  const handleRechazar = (id, score) => {
-      axios.put(`https://api.restaurantepinochozaragoza.es/api/reservations/${id}/cancel`, { status: 'rechazado', score })
+  const handleRechazar = (id) => {
+      axios.put(`https://api.restaurantepinochozaragoza.es/api/reservations/${id}/cancel`, { status: 'rechazado' })
           .then(response => {
               console.log('Reserva rechazada:', response.data);
               Swal.fire(
@@ -44,7 +44,7 @@ const GestionReservas = () => {
                 'Reserva rechazada con éxito',
                 'Recibirás un correo con la información de la reserva',
               );
-              getAceptadas();
+              setReservasPendientes(reservasPendientes.filter(reserva => reserva.id !== id));
               getRechazadas();
           })
           .catch(error => {
@@ -70,42 +70,73 @@ const GestionReservas = () => {
 
     return (
       <div className={Style.gestionContainer}>
-        <h1 className={Style.title}>Reservas pendientes</h1>
+    <h1 className={Style.title}>Reservas pendientes</h1>
     <div className={Style.reservasContainer}>
-      
-    {reservasPendientes.map(reserva => (
-  <div key={reserva.id} className={Style.gestion}>
-    <p className={Style.text}>Nombre: {reserva.firstName}</p>
-    <p className={Style.text}>Personas: {reserva.people}</p>
-    <p className={Style.text}>Fecha: {reserva.date}</p>
-    <p className={Style.text}>Teléfono: {reserva.phone}</p>
-    <p className={Style.text}>Email: {reserva.email}</p>
-    <p className={Style.text}>Hora: {reserva.time}</p>
-    <p className={Style.text}>Alergias: {reserva.allergies}</p>
-    <p className={Style.text}>Puntos: {reserva.score}</p>
-    {reserva.status === 'pendiente' && (
-      <div className={Style.ContainerButton}>
-        <button className={Style.orangeButton} onClick={() => handleAceptar(reserva.id)}>Aceptar</button>
-        <button className={Style.blackButton} onClick={() => handleRechazar(reserva.id)}>Rechazar</button>
-      </div>
-    )}
-    {(reserva.status === 'aceptado' || reserva.status === 'cancelado') && (
-      <div className={Style.containerButton}>
-        <input type="number" value={scoreChange[reserva.id] || ''} onChange={e => setScoreChange({...scoreChange, [reserva.id]: Number(e.target.value)})} />
-    <button onClick={() => handleGuardarScore(reserva.id)}>Guardar</button>
-      </div>
-    )}
-  </div>
-))}
-    </div>
-    <div>
-          <div className={Style.ContainerButton}>
-    <button className={Style.Button} onClick={getAceptadas}>{mostrarAceptadas ? 'Ocultar reservas aceptadas' : 'Mostrar reservas aceptadas'}</button>
-    <button className={Style.Button} onClick={getRechazadas}>{mostrarRechazadas ? 'Ocultar reservas rechazadas' : 'Mostrar reservas rechazadas'}</button>
-</div>
-              
+      {reservasPendientes.map(reserva => (
+        <div key={reserva.id} className={Style.gestion}>
+          <p className={Style.text}>Nombre: {reserva.firstName}</p>
+          <p className={Style.text}>Personas: {reserva.people}</p>
+          <p className={Style.text}>Fecha: {reserva.date}</p>
+          <p className={Style.text}>Teléfono: {reserva.phone}</p>
+          <p className={Style.text}>Email: {reserva.email}</p>
+          <p className={Style.text}>Hora: {reserva.time}</p>
+          <p className={Style.text}>Alergias: {reserva.allergies}</p>
+          <p className={Style.text}>Puntos: {reserva.score}</p>
+          <div className={Style.containerButton}>
+            <button className={Style.Button} onClick={() => handleAceptar(reserva.id)}>Aceptar</button>
+            <button className={Style.Button} onClick={() => handleRechazar(reserva.id)}>Rechazar</button>
           </div>
+        </div>
+      ))}
+    </div>
+
+    <h1 className={Style.title}>Reservas aceptadas</h1>
+    <div className={Style.reservasContainer}>
+      {mostrarAceptadas && reservasAceptadas.map(reserva => (
+        <div key={reserva.id} className={Style.gestion}>
+          <p className={Style.text}>Nombre: {reserva.firstName}</p>
+          <p className={Style.text}>Personas: {reserva.people}</p>
+          <p className={Style.text}>Fecha: {reserva.date}</p>
+          <p className={Style.text}>Teléfono: {reserva.phone}</p>
+          <p className={Style.text}>Email: {reserva.email}</p>
+          <p className={Style.text}>Hora: {reserva.time}</p>
+          <p className={Style.text}>Alergias: {reserva.allergies}</p>
+          <p className={Style.text}>Puntos: {reserva.score}</p>
+          <div className={Style.containerButton}>
+            <input type="number" value={scoreChange[reserva.id] || ''} onChange={e => setScoreChange({...scoreChange, [reserva.id]: Number(e.target.value)})} />
+            <button onClick={() => handleGuardarScore(reserva.id)}>Guardar</button>
+          </div>
+        </div>
+      ))}
+    </div>
+
+    <h1 className={Style.title}>Reservas rechazadas</h1>
+    <div className={Style.reservasContainer}>
+      {mostrarRechazadas && reservasRechazadas.map(reserva => (
+        <div key={reserva.id} className={Style.gestion}>
+          <p className={Style.text}>Nombre: {reserva.firstName}</p>
+          <p className={Style.text}>Personas: {reserva.people}</p>
+          <p className={Style.text}>Fecha: {reserva.date}</p>
+          <p className={Style.text}>Teléfono: {reserva.phone}</p>
+          <p className={Style.text}>Email: {reserva.email}</p>
+          <p className={Style.text}>Hora: {reserva.time}</p>
+          <p className={Style.text}>Alergias: {reserva.allergies}</p>
+          <p className={Style.text}>Puntos: {reserva.score}</p>
+          <div className={Style.containerButton}>
+            <input type="number" value={scoreChange[reserva.id] || ''} onChange={e => setScoreChange({...scoreChange, [reserva.id]: Number(e.target.value)})} />
+            <button onClick={() => handleGuardarScore(reserva.id)}>Guardar</button>
+          </div>
+        </div>
+      ))}
+    </div>
+
+    <div>
+      <div className={Style.ContainerButton}>
+        <button className={Style.Button} onClick={getAceptadas}>{mostrarAceptadas ? 'Ocultar reservas aceptadas' : 'Mostrar reservas aceptadas'}</button>
+        <button className={Style.Button} onClick={getRechazadas}>{mostrarRechazadas ? 'Ocultar reservas rechazadas' : 'Mostrar reservas rechazadas'}</button>
       </div>
-  );
-          };
+    </div>
+  </div>
+);
+};
 export default GestionReservas;
