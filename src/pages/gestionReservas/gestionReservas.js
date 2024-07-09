700b6615bb69bbcc2f23a712437e78f8cf7664d2
unknown <ardila0212@gmail.com>
2024-07-09 11:01:55 +0200

    searchDate

--- src/pages/gestionReservas/gestionReservas.js
+++ src/pages/gestionReservas/gestionReservas.js
@@ -147,60 +147,58 @@ const detallesReserva = [
 
     return (
       <div className={Style.gestionContainer}>
-        <h1 className={Style.title}>Reservas pendientes</h1>
-    <div className={Style.reservasContainer}>
-      
-    {reservasFiltradas.filter(reserva => reserva.status === 'pendiente').map(reserva => (
-  <div key={reserva.id} className={Style.gestion}>
-    <p className={Style.text}>Nombre: {reserva.firstName}</p>
-    <p className={Style.text}>Personas: {reserva.people}</p>
-    <p className={Style.text}>Fecha: {reserva.date}</p>
-    <p className={Style.text}>Teléfono: {reserva.phone}</p>
-    <p className={Style.text}>Email: {reserva.email}</p>
-    <p className={Style.text}>Hora: {reserva.time}</p>
-    <p className={Style.text}>Alergias: {reserva.allergies}</p>
-    <p className={Style.text}>Puntos: {reserva.score}</p>
-    {reserva.status === 'pendiente' && (
-      <div className={Style.ContainerButton}>
-        <button className={Style.orangeButton} onClick={() => handleAceptar(reserva.id)}>Aceptar</button>
-        <button className={Style.blackButton} onClick={() => handleRechazar(reserva.id)}>Rechazar</button>
-      </div>
-    )}
-    {(reserva.status === 'aceptado' || reserva.status === 'cancelado') && (
-      <div className={Style.containerButton}>
-        <input type="number" value={scoreChange[reserva.id] || ''} onChange={e => setScoreChange({...scoreChange, [reserva.id]: Number(e.target.value)})} />
-    <button onClick={() => handleGuardarScore(reserva.id)}>Guardar</button>
-      </div>
-    )}
-  </div>
-))}
-    </div>
-    <div>
-    <div>
-      <input
-        value={searchDate}
-        onChange={(e) => setSearchDate(e.target.value)}
-        type="date"
-        placeholder="Buscar por fecha"
-      />
-      <button onClick={searchReservations}>Buscar</button>
-      {reservations.length > 0 && (
-        <div>
-         {detallesReserva.map((detalle) => (
-      <p className={Style.text} key={detalle.etiqueta}>
-        {detalle.etiqueta}: {detalle.valor}
-      </p>
-    ))}
-        </div>
-      )}
-    </div>
-          <div className={Style.ContainerButton}>
-    <button className={Style.Button} onClick={getAceptadas}>{mostrarAceptadas ? 'Ocultar reservas aceptadas' : 'Mostrar reservas aceptadas'}</button>
-    <button className={Style.Button} onClick={getRechazadas}>{mostrarRechazadas ? 'Ocultar reservas rechazadas' : 'Mostrar reservas rechazadas'}</button>
-</div>
-              
+      <h1 className={Style.title}>Reservas pendientes</h1>
+      <div className={Style.reservasContainer}>
+        {reservasFiltradas.filter(reserva => reserva.status === 'pendiente').map(reserva => (
+          <div key={reserva.id} className={Style.gestion}>
+            <p className={Style.text}>Nombre: {reserva.firstName}</p>
+            <p className={Style.text}>Personas: {reserva.people}</p>
+            <p className={Style.text}>Fecha: {reserva.date}</p>
+            <p className={Style.text}>Teléfono: {reserva.phone}</p>
+            <p className={Style.text}>Email: {reserva.email}</p>
+            <p className={Style.text}>Hora: {reserva.time}</p>
+            <p className={Style.text}>Alergias: {reserva.allergies}</p>
+            <p className={Style.text}>Puntos: {reserva.score}</p>
+            {reserva.status === 'pendiente' && (
+              <div className={Style.ContainerButton}>
+                <button className={Style.orangeButton} onClick={() => handleAceptar(reserva.id)}>Aceptar</button>
+                <button className={Style.blackButton} onClick={() => handleRechazar(reserva.id)}>Rechazar</button>
+              </div>
+            )}
+            {(reserva.status === 'aceptado' || reserva.status === 'cancelado') && (
+              <div className={Style.containerButton}>
+                <input type="number" value={scoreChange[reserva.id] || ''} onChange={e => setScoreChange({...scoreChange, [reserva.id]: Number(e.target.value)})} />
+                <button onClick={() => handleGuardarScore(reserva.id)}>Guardar</button>
+              </div>
+            )}
           </div>
+        ))}
       </div>
+      <div>
+        <div>
+          <input
+            value={searchDate}
+            onChange={(e) => setSearchDate(e.target.value)}
+            type="date"
+            placeholder="Buscar por fecha"
+          />
+          <button onClick={searchReservations}>Buscar</button>
+          {reservations.length > 0 && (
+            <div>
+              {detallesReserva.map((detalle) => (
+                <p className={Style.text} key={detalle.etiqueta}>
+                  {detalle.etiqueta}: {detalle.valor}
+                </p>
+              ))}
+            </div>
+          )}
+        </div>
+        <div className={Style.ContainerButton}>
+          <button className={Style.Button} onClick={getAceptadas}>{mostrarAceptadas ? 'Ocultar reservas aceptadas' : 'Mostrar reservas aceptadas'}</button>
+          <button className={Style.Button} onClick={getRechazadas}>{mostrarRechazadas ? 'Ocultar reservas rechazadas' : 'Mostrar reservas rechazadas'}</button>
+        </div>
+      </div>
+    </div>
   );
           };
 export default GestionReservas;
