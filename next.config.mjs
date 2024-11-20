835e758788ed6b1856a9527f420c06314d4ddc4b
unknown <ardila0212@gmail.com>
2024-11-20 10:22:12 +0100

    redirection!

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    async redirects() {
+      return [
+        {
+          source: '/:path*', // Coincide con cualquier ruta
+          has: [
+            {
+              type: 'host',
+              value: 'restaurantepinochozaragoza.es', // Dominio sin 'www'
+            },
+          ],
+          destination: 'https://www.restaurantepinochozaragoza.es/:path*', // Redirige a 'www'
+          permanent: true, // Código 301
+        },
+      ]; // Cierra el arreglo de redirecciones
+    },
+  };
+  
+  export default nextConfig;
+  
