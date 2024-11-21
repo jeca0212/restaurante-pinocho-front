a6df02d4cc00d322bf16dce6c531f0e8b3381715
unknown <ardila0212@gmail.com>
2024-11-21 15:24:11 +0100

    delete redirection

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    async redirects() {
-      return [
-        {
-          source: '/:path*', // Coincide con cualquier ruta
-          has: [
-            {
-              type: 'host',
-              value: 'restaurantepinochozaragoza.es', // Dominio sin 'www'
-            },
-          ],
-          destination: 'https://www.restaurantepinochozaragoza.es/:path*', // Redirige a 'www'
-          permanent: true, // Código 301
-        },
-      ]; // Cierra el arreglo de redirecciones
-    },
-  };
+const nextConfig = {}
+  //   async redirects() {
+  //     return [
+  //       {
+  //         source: '/:path*', // Coincide con cualquier ruta
+  //         has: [
+  //           {
+  //             type: 'host',
+  //             value: 'restaurantepinochozaragoza.es', // Dominio sin 'www'
+  //           },
+  //         ],
+  //         destination: 'https://www.restaurantepinochozaragoza.es/:path*', // Redirige a 'www'
+  //         permanent: true, // Código 301
+  //       },
+  //     ]; // Cierra el arreglo de redirecciones
+  //   },
+  // };
   
   export default nextConfig;
   
